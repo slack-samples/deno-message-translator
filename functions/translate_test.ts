73bb20ed3312d2bf8dde920e139da6570c96e0d3
William Bergamin <wbergamin@slack-corp.com>
2025-05-28 10:33:36 -0400

    simplify stub fetch

--- functions/translate_test.ts
+++ functions/translate_test.ts
@@ -1,115 +1,100 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
-import { assert, assertEquals, assertNotEquals } from "@std/assert";
+import { assertEquals } from "@std/assert";
 import handler from "./translate.ts";
-import { StubFetch } from "../testing/http.ts";
+import { stub } from "@std/testing/mock";
 
-// Replaces globalThis.fetch with the stub copy
-const stubFetch = new StubFetch();
-stubFetch.stub({
-  matches: (req) => {
-    assertEquals(req.method, "POST");
-    assertEquals(
-      req.url,
-      "https://slack.com/api/conversations.replies",
-    );
-    assertEquals(req.headers.get("Authorization"), "Bearer empty-response");
-  },
-  response: new Response(JSON.stringify({ ok: true, messages: [] }), {
-    status: 200,
-  }),
-});
+function stubFetch() {
+  // Replaces globalThis.fetch with the mocked copy
+  return stub(
+    globalThis,
+    "fetch",
+    async (url: string | URL | Request, options?: RequestInit) => {
+      const req = url instanceof Request ? url : new Request(url, options);
 
-stubFetch.stub({
-  matches: (req) => {
-    assertEquals(req.method, "POST");
-    assertEquals(
-      req.url,
-      "https://slack.com/api/conversations.replies",
-    );
-    assertNotEquals(req.headers.get("Authorization"), "Bearer empty-response");
-  },
-  response: new Response(
-    JSON.stringify({
-      "ok": true,
-      "oldest": "1670566778.964519",
-      "messages": [
-        {
-          "type": "message",
-          "text":
-            "Make work life simpler, more pleasant and more productive.\n\nSlack is the collaboration hub that brings the right people, information, and tools together to get work done. From Fortune 100 companies to corner markets, millions of people around the world use Slack to connect their teams, unify their systems, and drive their business forward.",
-          "user": "U03E94MK0",
-          "ts": "1670566778.964519",
-          "team": "T03E94MJU",
-          "thread_ts": "1670566778.964519",
-          "reply_count": 3,
-          "reply_users_count": 1,
-          "latest_reply": "1670570301.090889",
-          "reply_users": ["U04EJMQQEFN"],
-          "is_locked": false,
-          "subscribed": false,
-          "reactions": [{ "name": "jp", "users": ["U03E94MK0"], "count": 1 }],
-        },
-      ],
-      "has_more": false,
-      "pin_count": 0,
-      "channel_actions_ts": null,
-      "channel_actions_count": 0,
-    }),
-    {
-      status: 200,
+      assertEquals(req.method, "POST");
+
+      switch (req.url) {
+        case "https://slack.com/api/conversations.replies":
+          if (req.headers.get("Authorization") === "Bearer empty-response") {
+            return new Response(JSON.stringify({ ok: true, messages: [] }), {
+              status: 200,
+            });
+          }
+          return new Response(
+            JSON.stringify({
+              "ok": true,
+              "oldest": "1670566778.964519",
+              "messages": [
+                {
+                  "type": "message",
+                  "text":
+                    "Make work life simpler, more pleasant and more productive.\n\nSlack is the collaboration hub that brings the right people, information, and tools together to get work done. From Fortune 100 companies to corner markets, millions of people around the world use Slack to connect their teams, unify their systems, and drive their business forward.",
+                  "user": "U03E94MK0",
+                  "ts": "1670566778.964519",
+                  "team": "T03E94MJU",
+                  "thread_ts": "1670566778.964519",
+                  "reply_count": 3,
+                  "reply_users_count": 1,
+                  "latest_reply": "1670570301.090889",
+                  "reply_users": ["U04EJMQQEFN"],
+                  "is_locked": false,
+                  "subscribed": false,
+                  "reactions": [{
+                    "name": "jp",
+                    "users": ["U03E94MK0"],
+                    "count": 1,
+                  }],
+                },
+              ],
+              "has_more": false,
+              "pin_count": 0,
+              "channel_actions_ts": null,
+              "channel_actions_count": 0,
+            }),
+            {
+              status: 200,
+            },
+          );
+        case "https://slack.com/api/chat.postMessage": {
+          return new Response(JSON.stringify({ ok: true, ts: "1111.2222" }), {
+            status: 200,
+          });
+        }
+        case "https://api.deepl.com/v2/translate": {
+          const body = await req.formData();
+          if (body.get("auth_key") === "valid") {
+            return new Response(
+              JSON.stringify({
+                translations: [
+                  {
+                    detected_source_language: "EN",
+                    text:
+                      "ワークライフをよりシンプルに、より快適に、より生産的にする。\n\nSlack は、適切な人、情報、ツールを集めて仕事を成し遂げるためのコラボレーション ハブです。フォーチュン100の企業から片隅の市場ま...",
+                  },
+                ],
+              }),
+              {
+                status: 200,
+              },
+            );
+          }
+          return new Response("", { status: 403 });
+        }
+        default:
+          throw Error(
+            `No stub found for ${req.method} ${req.url}\nHeaders: ${
+              JSON.stringify(Object.fromEntries(req.headers.entries()))
+            }`,
+          );
+      }
     },
-  ),
-});
-
-stubFetch.stub({
-  matches: (req) => {
-    assertEquals(req.method, "POST");
-    assertEquals(
-      req.url,
-      "https://slack.com/api/chat.postMessage",
-    );
-  },
-  response: new Response(JSON.stringify({ ok: true, ts: "1111.2222" }), {
-    status: 200,
-  }),
-});
-
-stubFetch.stub({
-  matches: async (req) => {
-    assertEquals(req.method, "POST");
-    assert(req.url.endsWith("/v2/translate"));
-    const body = await req.formData();
-    assertNotEquals(body.get("auth_key"), "valid");
-  },
-  response: new Response("", { status: 403 }),
-});
-
-stubFetch.stub({
-  matches: async (req) => {
-    assertEquals(req.method, "POST");
-    assert(req.url.endsWith("/v2/translate"));
-    const body = await req.formData();
-    assertEquals(body.get("auth_key"), "valid");
-  },
-  response: new Response(
-    JSON.stringify({
-      translations: [
-        {
-          detected_source_language: "EN",
-          text:
-            "ワークライフをよりシンプルに、より快適に、より生産的にする。\n\nSlack は、適切な人、情報、ツールを集めて仕事を成し遂げるためのコラボレーション ハブです。フォーチュン100の企業から片隅の市場ま...",
-        },
-      ],
-    }),
-    {
-      status: 200,
-    },
-  ),
-});
+  );
+}
 
 const { createContext } = SlackFunctionTester("my-function");
 
 Deno.test("No message found", async () => {
+  using _stubFetch = stubFetch();
   const inputs = {
     channelId: "C111",
     messageTs: "1670566778.964519",
@@ -122,6 +107,7 @@ Deno.test("No message found", async () => {
 });
 
 Deno.test("Translate a message successfully", async () => {
+  using _stubFetch = stubFetch();
   const inputs = {
     channelId: "C123",
     messageTs: "1670566778.964519",
@@ -134,6 +120,7 @@ Deno.test("Translate a message successfully", async () => {
 });
 
 Deno.test("Fail to translate with an invalid auth key", async () => {
+  using _stubFetch = stubFetch();
   const inputs = {
     channelId: "C111",
     messageTs: "1670566778.964519",
